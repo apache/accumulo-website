9c36c0ff8734f185a5c07b54489d81082b943862
Mike Walch <mwalch@apache.org>
2019-02-06 17:34:33 -0500

    Update tour code for 2.0

--- src/main/java/tour/Main.java
+++ src/main/java/tour/Main.java
@@ -1,38 +1,56 @@
 package tour;
 
 // Classes you will use along the tour
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+
+import org.apache.accumulo.core.client.Accumulo;
+import org.apache.accumulo.core.client.AccumuloClient;
+import org.apache.accumulo.core.client.BatchScanner;
 import org.apache.accumulo.core.client.BatchWriter;
 import org.apache.accumulo.core.client.BatchWriterConfig;
-import org.apache.accumulo.core.client.Connector;
+import org.apache.accumulo.core.client.ConditionalWriter;
+import org.apache.accumulo.core.client.ConditionalWriterConfig;
+import org.apache.accumulo.core.client.IsolatedScanner;
+import org.apache.accumulo.core.client.IteratorSetting;
 import org.apache.accumulo.core.client.MutationsRejectedException;
 import org.apache.accumulo.core.client.Scanner;
 import org.apache.accumulo.core.client.TableNotFoundException;
+import org.apache.accumulo.core.client.security.tokens.PasswordToken;
+import org.apache.accumulo.core.data.Condition;
+import org.apache.accumulo.core.data.ConditionalMutation;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.Mutation;
+import org.apache.accumulo.core.data.Range;
 import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.security.Authorizations;
+import org.apache.accumulo.core.security.ColumnVisibility;
+import org.apache.accumulo.core.security.TablePermission;
 import org.apache.accumulo.minicluster.MiniAccumuloCluster;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+import java.util.function.Function;
 
 public class Main {
+
+    static void exercise(MiniAccumuloCluster mac) throws Exception {
+        // start writing your code here
+    }
 
     public static void main(String[] args) throws Exception {
         System.out.println("Running the Accumulo tour. Having fun yet?");
 
         Path tempDir = Files.createTempDirectory(Paths.get("target"), "mac");
-        MiniAccumuloCluster mac = new MiniAccumuloCluster(tempDir.toFile(), "tourguide");
+        MiniAccumuloCluster mac = new MiniAccumuloCluster(tempDir.toFile(), "tourpasswd");
 
         mac.start();
         exercise(mac);
         mac.stop();
-    }
-
-    static void exercise(MiniAccumuloCluster mac) throws Exception{
-        // start writing your code here
-
     }
 }
