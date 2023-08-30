53eb13d24aa9f99757f4e0d6891fb82d54044f12
Dom G <domgarguilo@apache.org>
2023-08-30 08:49:38 -0400

    Update search page dependencies (#399)
    * Update search page dependencies
    
    ---------
    
    Co-authored-by: Christopher Tubbs <ctubbsii@apache.org>

--- js/search.js
+++ js/search.js
@@ -1,48 +1,47 @@
-jQuery(function() {
+jQuery(function () {
 
-  window.idx = lunr(function () {
-    this.field('id');
-    this.field('title');
-    this.field('content', { boost: 10 });
-    this.field('categories');
-  });
+  $.getJSON('/search_data.json').then(function (loaded_data) {
 
-  window.data = $.getJSON('/search_data.json');
+    window.idx = lunr(function () {
+      this.field('id');
+      this.field('title');
+      this.field('content', { boost: 10 });
+      this.field('categories');
 
-  window.data.then(function(loaded_data){
-    $.each(loaded_data, function(index, value){
-      window.idx.add($.extend({ "id": index }, value));
+      for (var index in loaded_data) {
+        this.add($.extend({ "id": index }, loaded_data[index]));
+      }
     });
-  });
 
-  $("#site_search").submit(function(event){
+    $("#site_search").submit(function (event) {
       event.preventDefault();
       var query = $("#search_box").val();
       var results = window.idx.search(query);
-      display_search_results(query, results);
+      display_search_results(query, results, loaded_data);
+    });
+
   });
 
-  function display_search_results(query, results) {
+  function display_search_results(query, results, loaded_data) {
     var $search_status = $("#search_status");
     var $search_results = $("#search_results");
 
-    window.data.then(function(loaded_data) {
+    if (!results.length) {
+      $search_status.html('No results found!');
+      return;
+    }
 
-      if (results.length) {
-        $search_status.html('Found ' + results.length + ' results for "' + query + '"');
-        $search_results.empty();
-        results.forEach(function(result) {
-          var item = loaded_data[result.ref];
-          var n = item.content.search(query) - 50
-          if (n < 0) {
-            n = 0;
-          }
-          var appendString = '<tr><td><a href="' + item.url + '">' + item.title + '</a><td>' + item.content.substring(n, n+100) + '</tr>';
-          $search_results.append(appendString);
-        });
-      } else {
-        $search_status.html('No results found!');
+    $search_status.html('Found ' + results.length + ' results for "' + query + '"');
+    $search_results.empty();
+    results.forEach(function (result) {
+      var item = loaded_data[result.ref];
+      var n = item.content.search(query) - 50
+      if (n < 0) {
+        n = 0;
       }
+      var appendString = '<tr><td><a href="' + item.url + '">' + item.title + '</a><td>' + item.content.substring(n, n + 100) + '</tr>';
+      $search_results.append(appendString);
     });
+
   }
 });
